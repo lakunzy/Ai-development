8f3106725acf8ca3328e82c19e12df89fd810c6e
lakunzy7 <lakunzy7@gmail.com>
2023-03-17 08:37:29 -0700

    Task 4 Hello  puts for my first c command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
-/**
- * main - this is the main function
+/*
+ * main - 4-puts.c
+ * description - Write a C program rhat prints exactly
  *
- * Return: always 0
- */
+ * Return: Always 0 (Success)
+*/
 
-int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle);
-        return (0);
+int main(void
+		)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
